da83d6c163e1b9e0d6c10f4ae24d885361625629
iampeterbanjo <peterbanjo@gmail.com>
2016-03-30 07:05:16 +0100

    add heroes and hero list

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,28 +5,50 @@ export class Hero {
   name: string;
 }
 
+var HEROES: Hero[] = [
+  { "id": 101, "name": "Edward Snowden" }
+  , { "id": 102, "name": "Amazingly Awesome" }
+  , { "id": 103, "name": "This Developer\'s Life" }
+  , { "id": 104, "name": "Alex Bloomberg" }
+  , { "id": 105, "name": "Scott Guthrie" }
+  , { "id": 106, "name": "Mama Dayo" }
+  , { "id": 107, "name": "Dr. Banjo" }
+  , { "id": 108, "name": "Mrs. M. Banjo" }
+  , { "id": 109, "name": "Stephen Dubner" }
+  , { "id": 110, "name": "Deep Throat" }
+]
+
 @Component({
-    selector: 'my-app',
-    template: `
-      <h1>{{title}}</h1>
-      <h2>{{hero.name}} details!</h2>
-      <div>
-        <label>id:</label> {{hero.id}}
-      </div>
-      <div>
-        <label>name:</label> {{hero.name}}
-      </div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
-      </div>
-    `
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+
+    <h2>My Heroes</h2>
+    <ul>
+      <li *ngFor="#hero of heroes">
+        <span class"badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+
+    <h2>{{hero.name}} details!</h2>
+    <div>
+      <label>id:</label> {{hero.id}}
+    </div>
+    <div>
+      <label>name:</label> {{hero.name}}
+    </div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+  `
 })
+
 export class AppComponent {
   public title = 'Tour of Heroes'
-
   public hero: Hero = {
     id: 1,
     name: 'Windstorm'
   }
+  public heroes = HEROES;
 }
